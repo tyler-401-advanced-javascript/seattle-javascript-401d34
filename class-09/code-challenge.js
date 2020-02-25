a1b63f8a5389e24ace98c6a4836c349a426fcc6e
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2020-02-25 12:11:36 -0800

    Add code challenge from class 09 - roman numerals

---
+++ class-09/code-challenge.js
@@ -0,0 +1,13 @@
+/*
+CHALLENGE: Write a function that takes in a string of Roman numerals
+and outputs a "traditional" integer.
+Examples:
+  Input      Output
+  VIII       8
+  IV         4
+  CLXVII     167
+  MCMXCIX    1999
+You don't need to validate that your input is a correctly formed
+Roman numeral: you may assume that it will be correctly formed.
+You also don't need to account for negatives or zeroes.
+*/
