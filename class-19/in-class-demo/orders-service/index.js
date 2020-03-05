3c400e2d0f4e8ec56e970ae9b5dd6fb0cedecfef
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2020-03-05 12:15:07 -0800

    Add in-class-demo of AWS/SQS/Mailgun

---
+++ class-19/in-class-demo/orders-service/index.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const AWS = require('aws-sdk')
+AWS.config.update({ region: 'us-west-2' })
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' })
+const { SQS_QUEUE_URL } = process.env
+
+// orders look like:
+// {
+//   userEmail: <email>
+//   itemName: <string>
+//   itemPrice: <number>
+//   itemQuantity: <number>
+// }
+
+app.post('/order', async (req, res) => {
+  const { userEmail, itemName, itemPrice, itemQuantity } = req.body
+  const sqsData = {
+    MessageAttributes: {
+      userEmail: {
+        DataType: 'String',
+        StringValue: userEmail
+      },
+      itemName: {
+        DataType: 'String',
+        StringValue: itemName
+      },
+      itemPrice: {
+        DataType: 'Number',
+        StringValue: itemPrice
+      },
+      itemQuantity: {
+        DataType: 'Number',
+        StringValue: itemQuantity
+      }
+    },
+    MessageBody: JSON.stringify(req.body),
+    MessageGroupId: 'UserOrders',
+    MessageDeduplicationId: userEmail,
+    QueueUrl: SQS_QUEUE_URL
+  }
+  try {
+    const response = await sqs.sendMessage(sqsData).promise()
+    console.log(`OrdersSerivce | SUCCESS: Message queued: ${response.MessageId}`)
+    res.json({ success: true, message: 'Thank you for your order.' })
+  } catch (err) {
+    console.error(`OrdersService | ERROR: ${err}`)
+    res.status(500).json({ success: false, message: 'oops' })
+  }
+})
+
+const ORDERS_SERVICE_PORT = process.env.ORDERS_SERVICE_PORT || 3000
+app.listen(ORDERS_SERVICE_PORT, () => console.log(`Orders service listening on ${ORDERS_SERVICE_PORT}`))
