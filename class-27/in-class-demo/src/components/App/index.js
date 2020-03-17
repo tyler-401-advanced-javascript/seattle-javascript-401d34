a49e79868a94447381b3fb30f07a44f571f773c5
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2020-03-17 14:59:27 -0700

    Add in-class-demo from 27

---
+++ class-27/in-class-demo/src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Thing from '../Thing'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My App</h1>
+      <Thing />
+    </div>
+  );
+}
+
+export default App;
